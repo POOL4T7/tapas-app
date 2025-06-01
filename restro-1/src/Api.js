3c38a8a2baef37b472872d73d0d03b4466ed29c2
Gulshan <abhinavg90834@gmail.com>
2025-06-01 19:39:55 +0530

    config

--- restro-1/src/Api.js
+++ restro-1/src/Api.js
@@ -1,5 +1,5 @@
 // const baseURL = 'https://backend.tapas-mundo.com/api';
-const baseURL = 'http://103.174.103.132:7250/altmariendorf/api';
+const baseURL = 'http://localhost:7250/altmariendorf/api';
 const menu_id = 1;
 const restaurant_id = 1;
 const LandingUrl = 'https://tapas-mundo.com';
